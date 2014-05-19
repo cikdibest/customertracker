d17f3dd4116c4f1071cfba095c46fd8675b617dd
cikdibest <cihandalgicdir@gmail.com>
2014-05-19 23:05:08 +0300

    Includes added.
    SearchResultModelId changed to url

--- src/CustomerTracker.Web/Business/SearchBusiness/IndexSearchEngine.cs
+++ src/CustomerTracker.Web/Business/SearchBusiness/IndexSearchEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using CustomerTracker.Web.Infrastructure.Repository;
 using CustomerTracker.Web.Models.Entities;
@@ -17,37 +18,36 @@ namespace CustomerTracker.Web.Business.SearchBusiness
 
         public IPagedList<SearchResultModel> SearchCustomers(string criteria, int currentPageIndex, string sorting, bool isAscending)
         {
-            var results = _unitOfWork.GetRepository<Customer>()
-                .Filter(c => c.Name.Contains("/" + criteria + "/"));
+            var results = _unitOfWork.GetRepository<Customer>().SelectAll().Include(cu=>cu.City)
+                .Where(c => c.Name.Contains(criteria));
             return ToPagedSearchResultModel<Customer>(results,currentPageIndex,20);
         }
 
         public IPagedList<SearchResultModel> SearchCommunications(string criteria, int currentPageIndex, string sorting, bool isAscending)
         {
-            var results = _unitOfWork.GetRepository<Communication>()
-                .Filter(c => c.FirstName.Contains("/" + criteria + "/") ||c.LastName.Contains("/"+criteria+"/") || (c.FirstName+" "+c.LastName).Contains("/"+criteria+"/"));
+            var results = _unitOfWork.GetRepository<Communication>().SelectAll().Include(co=>co.Customer).Include(co=>co.Customer.City)
+                .Where(c => c.FirstName.Contains(criteria) ||c.LastName.Contains(criteria) || (c.FirstName+" "+c.LastName).Contains(criteria));
             return ToPagedSearchResultModel<Communication>(results, currentPageIndex, 20);
         }
 
         public IPagedList<SearchResultModel> SearchRemoteComputers(string criteria, int currentPageIndex, string sorting, bool isAscending)
         {
-            var results = _unitOfWork.GetRepository<RemoteComputer>()
-                .Filter(c => c.Name.Contains("/" + criteria + "/"));
+            var results = _unitOfWork.GetRepository<RemoteComputer>().SelectAll().Include(re=>re.Customer).Include(re=>re.Customer.City)
+                .Where(c => c.Name.Contains(criteria));
             return ToPagedSearchResultModel<RemoteComputer>(results, currentPageIndex, 20);
         }
 
         private IPagedList<SearchResultModel> ToPagedSearchResultModel<T>(IQueryable<T> results, int pageIndex, int pageSize) where T : BaseEntity
         {
-            var type = typeof(T).ToString();
+            var type = typeof(T).Name;
             var searchResultList = new List<SearchResultModel>();
             switch (type)
             {
                 case "Customer":
-
                     foreach (var customer in results)
                     {
                         var castedCustomer = customer as Customer;
-                        searchResultList.Add(new SearchResultModel() { SearchResultId = customer.Id, Summary = castedCustomer.Explanation, Title = castedCustomer.Name + "-" + castedCustomer.City.Name });
+                        searchResultList.Add(new SearchResultModel() { Url = "/customer/"+customer.Id, Summary = castedCustomer.Explanation, Title = castedCustomer.Name + "-" + castedCustomer.City.Name });
                     }
                     return searchResultList.ToPagedList(pageIndex, pageSize);
 
@@ -57,7 +57,7 @@ namespace CustomerTracker.Web.Business.SearchBusiness
                         var castedCommunication = communication as Communication;
                         searchResultList.Add(new SearchResultModel()
                         {
-                            SearchResultId = communication.Id,
+                            Url ="/communication/"+ communication.Id,
                             Summary =
                                 castedCommunication.Customer.Name + "-" + castedCommunication.Customer.City.Name +
                                 "Tel : " + castedCommunication.PhoneNumber + "Email :" + castedCommunication.Email,
@@ -71,7 +71,7 @@ namespace CustomerTracker.Web.Business.SearchBusiness
                         var castedRemoteComputer = communication as RemoteComputer;
                         searchResultList.Add(new SearchResultModel()
                         {
-                            SearchResultId = communication.Id,
+                            Url = "/remotecomputer/"+castedRemoteComputer.Id,
                             Summary =
                                 castedRemoteComputer.Customer.Name + "-" + castedRemoteComputer.Customer.City.Name + "-" +
                                 castedRemoteComputer.RemoteConnectionType.ToString() + "Kullanıcı adı : " + castedRemoteComputer.Username + " Şifre :" + castedRemoteComputer.Password
