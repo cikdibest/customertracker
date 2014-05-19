a7a5748fb4ce3ceb7692ec0eae08af4e77187e9c
seyhan <syhnbkr@gmail.com>
2014-05-19 21:38:19 +0300

    pagin implemnation

--- src/CustomerTracker.Web/Controllers/LandingPageController.cs
+++ src/CustomerTracker.Web/Controllers/LandingPageController.cs
@@ -1,16 +1,45 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using CustomerTracker.Web.Utilities;
+using MvcPaging;
 
 namespace CustomerTracker.Web.Controllers
 {
     [Authorize(Roles = "Personel,Admin")]
     public class LandingPageController : Controller
     {
+        private ISearchEngine _searchEngine;
+
+        public LandingPageController()
+        {
+            _searchEngine = default(ISearchEngine);
+        }
+
         public ActionResult Index()
         {
             return View();
         }
 
       
+    }
+
+    public interface ISearchEngine
+    {
+        IPagedList<SearchResultModel> SearchCustomers(string criteria,int currentPageIndex,  string sorting, bool isAscending);
+
+        IPagedList<SearchResultModel> SearchCommunications(string criteria, int currentPageIndex, string sorting, bool isAscending);
+
+        IPagedList<SearchResultModel> SearchRemoteComputers(string criteria, int currentPageIndex, string sorting, bool isAscending);
+
+    }
+
+
+    public class SearchResultModel
+    {
+        public string Title { get; set; }
+
+        public int SearchResultId { get; set; }
+
+        public string Summary { get; set; }
     }
 }
