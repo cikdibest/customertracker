5c2068a97611925c654b3d2b568167aee5c770ae
seyhan <syhnbkr@gmail.com>
2014-06-01 22:24:36 +0300

    loading panel is added during ajax call

--- src/CustomerTracker.Web/Scripts/domain/appControllers.js
+++ src/CustomerTracker.Web/Scripts/domain/appControllers.js
@@ -68,8 +68,38 @@ var communicationApiUrl = {
     deletecommunication: '/api/communicationapi/deletecommunication/',
 };
 
+angular.module('SharedServices', [])
+    .config(function ($httpProvider) {
+        $httpProvider.responseInterceptors.push('myHttpInterceptor');
+        var spinnerFunction = function (data, headersGetter) {
+            // todo start the spinner here
+            //alert('start spinner');
+            $('#mydiv').show();
+            return data;
+        };
+        $httpProvider.defaults.transformRequest.push(spinnerFunction);
+    })
+// register the interceptor as a service, intercepts ALL angular ajax http calls
+    .factory('myHttpInterceptor', function ($q, $window) {
+        return function (promise) {
+            return promise.then(function (response) {
+                // do something on success
+                // todo hide the spinner
+                //alert('stop spinner');
+                $('#mydiv').hide();
+                return response;
 
-var customerApp = angular.module('customerApp', ['ui.bootstrap']);
+            }, function (response) {
+                // do something on error
+                // todo hide the spinner
+                //alert('stop spinner');
+                $('#mydiv').hide();
+                return $q.reject(response);
+            });
+        };
+    });
+
+var customerApp = angular.module('customerApp', ['ui.bootstrap', 'SharedServices']);
 
 var modalService = function ($modal) {
 
