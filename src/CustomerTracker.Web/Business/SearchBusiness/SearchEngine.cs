be48d5f5519dc52df2243e0ead804dda9aea110c
seyhan <syhnbkr@gmail.com>
2014-06-01 10:09:52 +0300

    removed comment code searchengin

--- src/CustomerTracker.Web/Business/SearchBusiness/SearchEngine.cs
+++ src/CustomerTracker.Web/Business/SearchBusiness/SearchEngine.cs
@@ -40,45 +40,6 @@ namespace CustomerTracker.Web.Business.SearchBusiness
             return list;
         }
 
-        //private IPagedList<dynamic> ToPagedSearchResultModel<T>(IQueryable<T> results, int pageIndex, int pageSize) where T : BaseEntity
-        //{
-        //    var type = typeof(T).Name;
-        //    var searchResultList = new List<SearchResultModel>();
-        //    switch (type.ToLower())
-        //    {
-        //        case "customer":
-        //            foreach (var customer in results)
-        //            {
-        //                var castedCustomer = customer as Customer;
-        //                searchResultList.Add(new SearchResultModel()
-        //                {
-        //                    SearchTypeId = SearchType.Customer.GetHashCode(), 
-        //                    Url = string.Format("{0}?customerId={1}", "api/customerapi/getcustomeradvanceddetail", customer.Id), 
-        //                    Summary = castedCustomer.Explanation,
-        //                    Title = castedCustomer.Name + "-" + castedCustomer.City.Name
-        //                });
-        //            }
-        //            return searchResultList.ToPagedList(pageIndex, pageSize);
-
-        //        case "communication":
-        //            foreach (var communication in results)
-        //            {
-        //                var castedCommunication = communication as Communication;
-        //                searchResultList.Add(new SearchResultModel()
-        //                {
-        //                    SearchTypeId = SearchType.Communication.GetHashCode(),
-        //                    Url = string.Format("{0}?customerId={1}", "api/customerapi/getcustomeradvanceddetail", castedCommunication.Customer.Id),
-        //                    Summary =
-        //                        castedCommunication.Customer.Name + "-" + castedCommunication.Customer.City.Name +
-        //                        "Tel : " + castedCommunication.MobilePhoneNumber + "Email :" + castedCommunication.Email,
-        //                    Title = castedCommunication.FirstName + castedCommunication.LastName
-        //                });
-        //            }
-        //            return searchResultList.ToPagedList(pageIndex, pageSize);
-
-        //        default:
-        //            return null;
-        //    }
-        //}
+        
     }
 }
