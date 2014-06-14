7651b211002633e26477c24542eb2011f674940d
seyhan <syhnbkr@gmail.com>
2014-06-15 01:42:01 +0300

    Update : misson is completed

---
+++ src/CustomerTracker.ApiService/Models/TargetService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CustomerTracker.ApiService.Models
+{
+ 
+   
+    public class TargetService
+    {
+        public int ApplicationServiceId { get; set; }
+
+        public string InstanceName { get; set; }
+
+        public int ApplicationServiceTypeId { get; set; }
+    }
+}
