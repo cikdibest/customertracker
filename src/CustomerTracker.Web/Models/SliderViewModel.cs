9b6da3ea085ba86b32a87d1aec2b573b455014b1
seyhan <syhnbkr@gmail.com>
2014-06-14 13:46:27 +0300

    Update : Remove unused class

--- src/CustomerTracker.Web/Models/SliderViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CustomerTracker.Web.Models
-{
-    //public class SliderViewModel
-    //{
-    //    public string Title { get; set; }
-    //    public string Text { get; set; }
-    //    public int ImageNo { get; set; }
-    //}
-}
